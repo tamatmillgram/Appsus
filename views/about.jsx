13bc4e9a84febbf18dadc6dbfe29f7de6657a601
Tamar Millgram <Tamarmillgram15@gmail.com>
2023-05-13 20:48:44 +0300

    Image path change

--- views/about.jsx
+++ views/about.jsx
@@ -4,6 +4,6 @@ export function About() {
         <h1 className="about-header">make communication better</h1>
         <p className="about-p">Email communication has revolutionized the way we connect and collaborate in the digital age. With just a few clicks, individuals and organizations can exchange messages, ideas, and information across vast distances and time zones. This efficient and reliable mode of communication has become an indispensable tool in both personal and professional realms. Email allows for swift and asynchronous communication, enabling individuals to send and receive messages
              at their convenience. It fosters effective collaboration, allowing multiple parties to be part of conversations, share documents, and provide timely feedback. Moreover, email provides a written record of correspondence, serving as a valuable reference for future interactions. With its versatility and accessibility, email has undoubtedly enhanced productivity, strengthened relationships, and facilitated seamless global communication.</p>
-        <img className="about-img" src="../../assets/img/about.jpg" alt="" />
+        <img className="about-img" src="./assets/img/about.jpg" alt="" />
     </section>
 }
